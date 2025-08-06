a926209cfd7392061417c096164ae98e167e11fd
jaabless <jefferyasamoahbless@gmail.com>
2025-08-06 23:07:36 +0000

    Create BasePage with common WebDriver utilities and Page Factory

---
+++ src/main/java/com/swaglabs/pages/BasePage.java
@@ -0,0 +1,43 @@
+package com.swaglabs.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    protected void waitForElementVisible(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected void waitForElementClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    protected void click(WebElement element) {
+        waitForElementClickable(element);
+        element.click();
+    }
+
+    protected void enterText(WebElement element, String text) {
+        waitForElementVisible(element);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getElementText(WebElement element) {
+        waitForElementVisible(element);
+        return element.getText();
+    }
+}
